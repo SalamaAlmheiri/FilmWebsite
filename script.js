1d57abeb103d58bdf8b454574ca4c19c1f18188e
SalamaAlmheiri <84767210+SalamaAlmheiri@users.noreply.github.com>
2022-02-17 00:53:26 +0400

    more edits

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 ///// Main page Javascript
 
+//Play
 var button= document.querySelector('.btn');
 var film= document.getElementById('film');
 
 button.addEventListener('click', function(){
 film.play();
-console.log("click the button")
+console.log("clicked play")
+});
+
+///Stop
+var button2= document.querySelector('.btn2');
+
+button2.addEventListener('click', function(){
+film.pause();
+console.log("clicked stop")
 });
